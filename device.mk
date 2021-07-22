31e17d18dd2cb7b2463cbcbb45c01883f53cfe3e
Danny Wood <danwood76@gmail.com>
2021-07-22 10:34:38 +0100

    j7xlte: add stock NFC implementation
    Change-Id: I6716110afa0ba52a50e135c700d732a104ba53fd

--- device.mk
+++ device.mk
@@ -63,8 +63,8 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 PRODUCT_PACKAGES += \
